81015efaa8b1255cc6c559e79cbd6c3a4cee09c3
Danielle Emygdio <emygdio.d@gmail.com>
2015-11-19 17:00:36 +1300

    Created method for rendering and loading. Still incompleted and it's causing a problem which is not allowing the images to be shown again, so something must be missing there.

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,4 +1,7 @@
+var board = [];
+
 window.onload = function() {
+	// hiding images used in the canvas
 	document.getElementById("exposedBlank").style.display = "none";
 	document.getElementById("exposedBomb").style.display = "none";
 	document.getElementById("exposedOne").style.display = "none";
@@ -8,9 +11,56 @@ window.onload = function() {
 	document.getElementById("hidden").style.display = "none";
 	document.getElementById("flagged").style.display = "none";
 
-	var canvas = document.getElementById("board"),
-		ctx = canvas.getContext("2d");
-		img = document.getElementById("hidden");
+	loadBoard();
+	renderBoard();
+}
 
-	ctx.drawImage(img, 10, 10);
+function loadBoard() {
+	board = [0, 0, 0, 0, 0];
+}
+
+function renderBoard() {
+	var image = "", i = 0;
+
+	for (i = 0; i < board.length; i++) {
+		switch() {
+			case 0:
+				// hidden square
+				image = "hidden";
+				break;
+			case 1:
+				// exposed blank square
+				image = "exposedBlank";
+				break;
+			case 2:
+				// exposed bomb
+				image = "exposedBomb";
+				break;
+			case 3:
+				// flagged square
+				image = "flagged";
+				break;
+			case 4:
+				// exposed one
+				image = "exposedOne";
+				break;
+			case 5:
+				// exposed two
+				image = "exposedTwo";
+				break;
+			case 6:
+				// exposed three
+				image = "exposedThree";
+				break;
+			case 7:
+				// exposed four
+				image = "exposedFour";
+				break;			
+		}
+
+		var canvas = document.getElementById("board"),
+		ctx = canvas.getContext("2d");
+		img = document.getElementById(image);
+		ctx.drawImage(img, i+30, i+30);
+	}	
 }
